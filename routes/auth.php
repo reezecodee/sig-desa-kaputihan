caa9cae6b0c20290242aae5eec9cbae738b4a38c
Reeze <azfaalharits25@gmail.com>
2025-02-12 00:10:44 +0700

    refactor: add middleware app-layout

--- routes/auth.php
+++ routes/auth.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\Auth\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('auth')->controller(AuthController::class)->group(function(){
+Route::prefix('auth')->middleware(['app-layout'])->controller(AuthController::class)->group(function(){
     Route::get('login', 'login')->name('auth.login');
     Route::post('proses-login', 'processLogin')->name('auth.loginProcess');
 });
