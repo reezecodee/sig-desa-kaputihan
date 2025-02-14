4815607dda2aef8c285b1628f1afa76135799b12
Reeze <azfaalharits25@gmail.com>
2025-02-14 23:17:02 +0700

    feat: change password

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -8,6 +8,7 @@ use App\Http\Requests\Admin\EditProfileRequest;
 use App\Services\UserService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use Inertia\Inertia;
 
 class ProfileController extends Controller
@@ -29,7 +30,7 @@ class ProfileController extends Controller
     public function editProfile(EditProfileRequest $request)
     {
         $id = Auth::user()->id;
-        
+
         try {
             $this->userService->update($request->validated(), $id);
 
@@ -43,6 +44,20 @@ class ProfileController extends Controller
 
     public function changePassword(ChangePasswordRequest $request)
     {
-        
+        $validated = $request->validated();
+
+        if (!Hash::check($validated['current_password'], Auth::user()->password)) {
+            return back()->withErrors([
+                'current_password' => 'Password saat ini tidak valid'
+            ]);
+        }
+
+        // Update password baru
+        $user = Auth::user();
+        $user->password = Hash::make($validated['new_password']);
+        $user->save();
+
+        session()->flash('success', 'Barhasil memperbarui password');
+        return Inertia::location(route('admin.profile'));
     }
 }
