12d60cf58b4cdad233430ac40cce2ea869564b8b
Reeze <azfaalharits25@gmail.com>
2025-02-19 15:50:13 +0700

    feat: add searching page

--- resources/js/Components/custom/Search.vue
+++ resources/js/Components/custom/Search.vue
@@ -1,13 +1,20 @@
 <script setup lang="ts">
 import { Input } from '@/Components/ui/input'
+import { ref } from 'vue'
+import { router } from '@inertiajs/vue3'
+
+const search = ref<string>('')
+
+const onSubmit = () => {
+  if (!search.value.trim()) return
+  router.visit(`/${encodeURIComponent(search.value)}`)
+}
 </script>
 
 <template>
   <div>
-    <Input
-      type="search"
-      placeholder="Search..."
-      class="md:w-[100px] lg:w-[300px]"
-    />
+    <form @submit.prevent="onSubmit">
+      <Input type="search" v-model="search" placeholder="Cari halaman..." class="md:w-[100px] lg:w-[300px]" />
+    </form>
   </div>
 </template>
